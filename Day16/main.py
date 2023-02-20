5e18f93fad8abe391a54f0207b9e9f1e3b3a6eb1
Edwin Aguilar <100698925+elaguila626@users.noreply.github.com>
2023-02-20 17:25:29 -0500

    Add files via upload

---
+++ Day16/main.py
@@ -0,0 +1,57 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# MenuItem is the class or blueprint
+# Item is the new object created
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+menu = Menu()
+#menu.get_items()
+#menu.find_drink(user_selects)
+#user_selects = input(f"What would you like {menu.get_items()}?")
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    user_selects = input(f"What would you like {options}?")
+    if user_selects == 'report':
+        money_machine.report()
+        coffee_maker.report()
+    elif user_selects == 'off':
+        is_on = False
+    else:
+        drink = menu.find_drink(user_selects)
+        if money_machine.make_payment(drink.cost) and coffee_maker.is_resource_sufficient(drink) == True:
+            coffee_maker.make_coffee(drink)
+        else:
+            is_on = False
+
+
+
+
+# coffee_maker.is_resource_sufficient(items)
+# money_machine.make_payment()
+#print(coffee_maker.is_resource_sufficient(items)) = True
+# coffee_maker.make_coffee(items)
+
+#items = MenuItem(user_selects,300,200,100,0)
+# print(items.name)
+# print(items.cost)
+# print(items.ingredients)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
