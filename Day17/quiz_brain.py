7b8378ef8758d162509e99807209ec7541214f7e
Edwin Aguilar <100698925+elaguila626@users.noreply.github.com>
2023-02-23 12:55:24 -0500

    Update quiz_brain.py

--- Day17/quiz_brain.py
+++ Day17/quiz_brain.py
@@ -1,7 +1,3 @@
-#Ask Question
-#Checking if the answer is correct
-#Checking if we're at the end of a quiz
-
 class QuizBrain:
 
     def __init__(self,q_list):
@@ -12,11 +8,6 @@ class QuizBrain:
 
     def still_has_questions(self):
         return self.question_number < len(self.questions_list)
-        # This is what I originally wrote:
-        # if self.question_number != 12:
-        #     return True
-        # else:
-        #     return False
 
     def next_question(self):
         current_question = self.questions_list[self.question_number]
@@ -33,19 +24,5 @@ class QuizBrain:
         print(f"The correct answer was: {correct_answer.lower()}")
         print(f"Your current score is {self.score}/{self.question_number}")
         print("\n")
-    # I started with this:
-    # def next_question(self,q_list, questions_answer):
-    #     user_answers = input(f"Q {q_list} (True/False)? ")
-    #     if questions_answer == user_answers:
-    #         self.question_number += 1
-    #         # user_score += 1
-    #         print("You got it right!")
-    #         print(f"The correct answer was: {questions_answer}")
-    #         print(f"Your current question numer is {self.question_number}")
-    #     else:
-    #         self.question_number += 1
-    #         #user_score += 0
-    #         print("You got it wrong!")
-    #         print(f"The correct answer was: {questions_answer}")
-    #         print(f"Your current question number is {self.question_number}")
+ 
 
